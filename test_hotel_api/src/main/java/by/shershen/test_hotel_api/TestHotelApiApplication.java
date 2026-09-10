361181d91707172b4e6db170014d65670f4b063f
Uladzislau Shershan <uladzislau.shershan.ext@sisal.it>
2026-09-11 01:00:11 +0400

    created project with spring boot initializr

---
+++ test_hotel_api/src/main/java/by/shershen/test_hotel_api/TestHotelApiApplication.java
@@ -0,0 +1,13 @@
+package by.shershen.test_hotel_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHotelApiApplication.class, args);
+	}
+
+}
---
+++ test_hotel_api/src/test/java/by/shershen/test_hotel_api/TestHotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shershen.test_hotel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
