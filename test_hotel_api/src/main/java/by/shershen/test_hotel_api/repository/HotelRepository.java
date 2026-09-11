9106830a1a83f80785c02048526f86779dde0147
Uladzislau Shershan <uladzislau.shershan.ext@sisal.it>
2026-09-12 01:31:32 +0400

    added HotelRepository queries

--- test_hotel_api/src/main/java/by/shershen/test_hotel_api/repository/HotelRepository.java
+++ test_hotel_api/src/main/java/by/shershen/test_hotel_api/repository/HotelRepository.java
@@ -3,6 +3,21 @@ package by.shershen.test_hotel_api.repository;
 import by.shershen.test_hotel_api.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long>, JpaSpecificationExecutor<Hotel> {
+
+    @Query("SELECT h.brand, COUNT(h) FROM Hotel h GROUP BY h.brand")
+    List<Object[]> countByBrand();
+
+    @Query("SELECT h.address.city, COUNT(h) FROM Hotel h GROUP BY h.address.city")
+    List<Object[]> countByCity();
+
+    @Query("SELECT h.address.country, COUNT(h) FROM Hotel h GROUP BY h.address.country")
+    List<Object[]> countByCountry();
+
+    @Query("SELECT a.name, COUNT(h) FROM Hotel h JOIN h.amenities a GROUP BY a.name")
+    List<Object[]> countByAmenity();
 }
