7ff1b4eebb26480743d972fd29f9d8c0af0c36af
Uladzislau Shershan <uladzislau.shershan.ext@sisal.it>
2026-09-11 21:44:09 +0400

    added repos

---
+++ test_hotel_api/src/main/java/by/shershen/test_hotel_api/repository/AmenityRepository.java
@@ -0,0 +1,11 @@
+package by.shershen.test_hotel_api.repository;
+
+import by.shershen.test_hotel_api.entity.Amenity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AmenityRepository extends JpaRepository<Amenity, Long> {
+
+    Optional<Amenity> findByNameIgnoreCase(String name);
+}
---
+++ test_hotel_api/src/main/java/by/shershen/test_hotel_api/repository/HotelRepository.java
@@ -0,0 +1,8 @@
+package by.shershen.test_hotel_api.repository;
+
+import by.shershen.test_hotel_api.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long>, JpaSpecificationExecutor<Hotel> {
+}
